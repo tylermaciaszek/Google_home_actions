a213557f2629b8ba40bcde9617755284d33b4cb7
Tyler Maciaszek <tmaciaszek10@gmail.com>
2017-02-21 19:45:22 -0500

    trying to fix error from home assistant

--- my_assistant/webook.py
+++ my_assistant/webook.py
@@ -1,8 +1,12 @@
+import logging
 from flask import Flask
-from flask_assistant import Assistant, tell
+from flask_assistant import Assistant, ask, tell, context_manager, HassRemote
+import homeassistant.remote as remote
 
 app = Flask(__name__)
-assist = Assistant(app, '/')
+assist = Assistant(app)
+logging.getLogger('flask_assistant').setLevel(logging.DEBUG)
+hass = HassRemote('YOUR Home Assistant PASSWORD')
 
 
 @assist.action('greeting')
@@ -10,22 +14,24 @@ def greet_and_start():
     speech = "Hey! Are you male or female?"
     return ask(speech)
 
-if __name__ = '__main__':
-    app.run(debug=True)
-	
-	@assist.action("user-gender")
+
+@assist.action("user-gives-gender")
 def ask_for_color(gender):
-    if gender is 'male':
+    if gender == 'male':
         gender_msg = 'Sup bro!'
+
     else:
         gender_msg = 'Haay gurl!'
 
     speech = gender_msg + ' What is your favorite color?'
     return ask(speech)
-	
-	@assist.action('give-color', mapping={'color': 'sys.color'})
-def ask_for_season(color):
-    speech = 'Ok, {} is an okay color I guess'.format(color)
+
+
+@assist.action('give-color', mapping={'color': 'sys.color'})
+def repeat_color(color):
+    speech = 'Ok, {} is an okay color I guess.'.format(color)
     return ask(speech)
-	
-	@assist.action('give-color', mapping={'color': 'sys.color'})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
